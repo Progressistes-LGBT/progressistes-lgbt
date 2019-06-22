556d56f2ff91b4d406db61fa950e7be2088b20b4
Marine Boudeau <m14868@gmail.com>
2019-06-22 08:44:42 +0200

    Tiny visual tweaks 🙈

--- components/Footer.js
+++ components/Footer.js
@@ -2,11 +2,7 @@ import Link from 'next/link';
 
 const Footer = () => (
   <div className="footer">
-  {/*
-    <Link href="/">
-      <a>Links</a>
-    </Link>
-  */}
+    Une association déposée le 18 juin 2019, et soutenue par <Link href="https://en-marche.fr"><a target="_blank">La République En Marche</a></Link> et <Link href="https://lesjams.fr"><a target="_blank">Les Jeunes avec Macron</a></Link>. 
   </div>
 );
 
